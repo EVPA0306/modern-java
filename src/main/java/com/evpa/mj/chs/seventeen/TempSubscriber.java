962ecb4ec8887b63d0e3311f38d41d70c4a98d0c
Eugene Pavlenko <C-Evgeny.Pavlenko@charter.com>
2018-07-18 12:12:04 -0600

    First commit in hibernate branch

---
+++ src/main/java/com/evpa/mj/chs/seventeen/TempSubscriber.java
@@ -0,0 +1,33 @@
+package com.evpa.mj.chs.seventeen;
+
+import org.reactivestreams.Subscriber;
+import org.reactivestreams.Subscription;
+
+public class TempSubscriber implements Subscriber<TempInfo> {
+
+    private Subscription subscription;
+
+
+    @Override
+    public void onSubscribe(Subscription subscription) {
+        this.subscription = subscription;
+        subscription.request(1);
+    }
+
+    @Override
+    public void onNext(TempInfo tempInfo) {
+        System.out.println(tempInfo);
+        subscription.request(1);
+    }
+
+    @Override
+    public void onError(Throwable t) {
+        System.err.println(t.getMessage());
+
+    }
+
+    @Override
+    public void onComplete() {
+        System.out.println("Done!");
+    }
+}
